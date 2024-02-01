d2b4dc2e429d248cdd09cda6b318361d9ae5d409
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-01 14:59:55 -0300

    feat: add the post repository

--- src/main/java/com/project/msblog/repositories/PostRepository.java
+++ src/main/java/com/project/msblog/repositories/PostRepository.java
@@ -1,2 +1,11 @@
-package com.project.msblog.repositories;public interface PostRepository {
+package com.project.msblog.repositories;
+
+import com.project.msblog.models.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
 }
