3b9486fc5cc4cfbd4b12b27e971c5c5c4df916d6
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-01 14:58:23 -0300

    feat: add the post entity

--- src/main/java/com/project/msblog/models/post/Post.java
+++ src/main/java/com/project/msblog/models/post/Post.java
@@ -1,2 +1,39 @@
-package com.project.msblog.models.post;public class Post {
+package com.project.msblog.models.post;
+
+import com.project.msblog.models.comment.Comment;
+import com.project.msblog.models.reader.Reader;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_posts")
+public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "author_id")
+  private Reader author;
+  private String title;
+  private String description;
+  private String category;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+  private List<Comment> comments;
+  private LocalDate publicationDate;
+  private LocalTime publicationTime;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 }
