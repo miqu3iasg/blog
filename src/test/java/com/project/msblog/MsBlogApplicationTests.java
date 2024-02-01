f6280594916986def3e863d8788c1033f49c4c04
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-01 11:59:39 -0300

    feat: initial commit

---
+++ src/test/java/com/project/msblog/MsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.msblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
