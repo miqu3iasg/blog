46844388167df5b8973ce441c037c32423857a65
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-01 14:57:56 -0300

    feat: add the comment entity

--- src/main/java/com/project/msblog/models/comment/Comment.java
+++ src/main/java/com/project/msblog/models/comment/Comment.java
@@ -1,2 +1,35 @@
-package com.project.msblog.models.comment;public class Comment {
+package com.project.msblog.models.comment;
+
+import com.project.msblog.models.post.Post;
+import com.project.msblog.models.reader.Reader;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_comments")
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "reader_id")
+  private Reader reader;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+  private LocalDate publicationDate;
+  private LocalTime publicationTime;
+
+  @ManyToOne
+  @JoinColumn(name = "post_id")
+  private Post post;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 }
