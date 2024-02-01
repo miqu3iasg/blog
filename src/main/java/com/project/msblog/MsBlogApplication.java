be0789340ab742fda4dcb8d1db32aa82758c2415
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-01 12:06:49 -0300

    feat: add initial folder structure

--- src/main/java/com/project/msblog/MsBlogApplication.java
+++ src/main/java/com/project/msblog/MsBlogApplication.java
@@ -2,8 +2,12 @@ package com.project.msblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.project.msblog")
+@EntityScan("com.project.msblog.models")
 public class MsBlogApplication {
 
 	public static void main(String[] args) {
