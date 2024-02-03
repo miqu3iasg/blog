477136c3b57ded0d1c0ee8fefe7bf998ddc06fa7
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 20:43:02 -0300

    feat: add reader DTO

--- src/main/java/com/project/msblog/dtos/ReaderDTO.java
+++ src/main/java/com/project/msblog/dtos/ReaderDTO.java
@@ -1,2 +1,26 @@
-package com.project.msblog.dtos;public class ReaderDTO {
+package com.project.msblog.dtos;
+
+import com.project.msblog.models.reader.ReaderRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ReaderDTO {
+  @NotNull
+  @Size(min = 2, max = 50)
+  private String username;
+  @NotNull
+  private String firstName;
+  @NotNull
+  private String lastName;
+  @Email
+  @NotNull
+  private String email;
+  @NotNull
+  private String password;
+  @NotNull
+  private ReaderRole role;
 }
