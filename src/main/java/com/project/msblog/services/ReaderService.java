714cfdac1b925f832a123ba609bccfd5927999a4
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 20:47:02 -0300

    feat: add method that searches a reader by username

--- src/main/java/com/project/msblog/services/ReaderService.java
+++ src/main/java/com/project/msblog/services/ReaderService.java
@@ -1,2 +1,9 @@
-package com.project.msblog.services;public interface ReaderService {
+package com.project.msblog.services;
+
+import com.project.msblog.models.reader.Reader;
+
+import java.util.Optional;
+
+public interface ReaderService {
+  Optional<Reader> findReaderByUsername(String username);
 }
--- src/main/java/com/project/msblog/services/ReaderServiceImplementation.java
+++ src/main/java/com/project/msblog/services/ReaderServiceImplementation.java
@@ -1,2 +1,21 @@
-package com.project.msblog.services;public class ReaderServiceImplementation {
+package com.project.msblog.services;
+
+import com.project.msblog.models.reader.Reader;
+import com.project.msblog.repositories.ReaderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReaderServiceImplementation implements ReaderService {
+  private final ReaderRepository readerRepository;
+
+  public ReaderServiceImplementation(ReaderRepository readerRepository) {
+    this.readerRepository = readerRepository;
+  }
+
+  @Override
+  public Optional<Reader> findReaderByUsername(String username) {
+    return readerRepository.findByUsername(username);
+  }
 }
