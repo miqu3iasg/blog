335b6859b32623093b5fefc70cfdda98e904ab41
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 20:48:19 -0300

    feat: add method that searches a post by id

--- src/main/java/com/project/msblog/services/PostService.java
+++ src/main/java/com/project/msblog/services/PostService.java
@@ -1,2 +1,10 @@
-package com.project.msblog.services;public class PostService {
+package com.project.msblog.services;
+
+import com.project.msblog.models.post.Post;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PostService {
+  Optional<Post> findPostById(UUID postId);
 }
--- src/main/java/com/project/msblog/services/PostServiceImplementation.java
+++ src/main/java/com/project/msblog/services/PostServiceImplementation.java
@@ -1,2 +1,22 @@
-package com.project.msblog.services;public class PostServiceImplementation {
+package com.project.msblog.services;
+
+import com.project.msblog.models.post.Post;
+import com.project.msblog.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PostServiceImplementation implements PostService {
+  private final PostRepository postRepository;
+
+  public PostServiceImplementation(PostRepository postRepository) {
+    this.postRepository = postRepository;
+  }
+
+  @Override
+  public Optional<Post> findPostById(UUID postId) {
+    return postRepository.findById(postId);
+  }
 }
