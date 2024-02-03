890b2791e764239755960ee2aab7ea08c8b37261
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 20:50:44 -0300

    feat: add method that publish a comment in a post

--- src/main/java/com/project/msblog/services/CommentService.java
+++ src/main/java/com/project/msblog/services/CommentService.java
@@ -1,2 +1,14 @@
-package com.project.msblog.services;public interface CommentService {
+package com.project.msblog.services;
+
+import com.project.msblog.dtos.CommentDTO;
+import com.project.msblog.models.comment.Comment;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CommentService {
+  Comment publishCommentOnPost(UUID postId, CommentDTO commentDataRequest);
+  Comment updateCommentContent(UUID commentId, String commentContentRequestForUpdate);
+  List<Comment> listAllCommentsOnThePost(UUID postId);
+  void removeCommentFromAPost(UUID commentId);
 }
--- src/main/java/com/project/msblog/services/CommentServiceImplementation.java
+++ src/main/java/com/project/msblog/services/CommentServiceImplementation.java
@@ -1,2 +1,74 @@
-package com.project.msblog.services;public class CommentServiceImplementation {
+package com.project.msblog.services;
+
+import com.project.msblog.dtos.CommentDTO;
+import com.project.msblog.models.comment.Comment;
+import com.project.msblog.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CommentServiceImplementation implements CommentService {
+  private final CommentRepository commentRepository;
+  private final ReaderService readerService;
+  private final PostService postService;
+
+  public CommentServiceImplementation(
+          CommentRepository commentRepository,
+          ReaderService readerService,
+          PostService postService) {
+    this.commentRepository = commentRepository;
+    this.readerService = readerService;
+    this.postService = postService;
+  }
+
+  @Override
+  public Comment publishCommentOnPost(UUID postId, CommentDTO commentDataRequest) {
+    return readerService.findReaderByUsername(commentDataRequest.getReaderUsername())
+            .map(readerFound -> postService.findPostById(postId)
+                    .map(postFound -> {
+                      Comment comment = new Comment();
+                      comment.setContent(commentDataRequest.getContent());
+                      comment.setReader(readerFound);
+                      comment.setPost(postFound);
+                      setCommentTimestamps(comment);
+
+                      return commentRepository.save(comment);
+                    }).orElseThrow(RuntimeException::new)
+            ).orElseThrow(RuntimeException::new);
+  }
+
+  private void setCommentTimestamps(Comment comment) {
+    comment.setPublicationDate(LocalDate.now());
+    comment.setPublicationTime(LocalTime.now());
+    comment.setCreatedAt(LocalDateTime.now());
+    comment.setUpdatedAt(LocalDateTime.now());
+  }
+
+  @Override
+  public Comment updateCommentContent(UUID commentId, String commentContentRequestForUpdate) {
+    
+  }
+
+  private void updateCommentTimestamps(Comment comment) {
+    comment.setPublicationDate(LocalDate.now());
+    comment.setPublicationTime(LocalTime.now());
+    comment.setUpdatedAt(LocalDateTime.now());
+  }
+
+  @Override
+  public List<Comment> listAllCommentsOnThePost(UUID postId) {
+
+  }
+
+  @Override
+  public void removeCommentFromAPost(UUID commentId) {
+
+  }
 }
+
+
