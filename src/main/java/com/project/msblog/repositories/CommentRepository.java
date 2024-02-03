6e9b89f9b73b655fc7a39ce803534bc7acbb2e85
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 20:45:23 -0300

    feat: add a method that list all comments in a specified post

--- src/main/java/com/project/msblog/repositories/CommentRepository.java
+++ src/main/java/com/project/msblog/repositories/CommentRepository.java
@@ -1,11 +1,14 @@
 package com.project.msblog.repositories;
 
 import com.project.msblog.models.comment.Comment;
+import com.project.msblog.models.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+  List<Comment> findByPost(Post post);
 }
