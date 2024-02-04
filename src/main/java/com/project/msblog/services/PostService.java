b0eb01c10af80fa8e9f5f6be2f7c0f709a1dad2e
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 22:18:14 -0300

    feat: add the signature of post service methods

--- src/main/java/com/project/msblog/services/PostService.java
+++ src/main/java/com/project/msblog/services/PostService.java
@@ -1,10 +1,20 @@
 package com.project.msblog.services;
 
+import com.project.msblog.dtos.PostDTO;
 import com.project.msblog.models.post.Post;
+import com.project.msblog.models.reader.Reader;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PostService {
+  Post createPost(PostDTO postDataRequestForCreate);
+  Post updatePostData(UUID postId, PostDTO postDataRequestForUpdate);
+  List<Post> listAllPosts();
+  Post searchJustOnePost(UUID postId);
+  Post searchPostByAuthor(Reader author);
+  Post searchPostByCategory(String category);
+  void removePost(UUID postId);
   Optional<Post> findPostById(UUID postId);
 }
