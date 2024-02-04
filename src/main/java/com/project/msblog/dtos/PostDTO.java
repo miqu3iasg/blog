1d2da771af3ef91c99e1a9c5cfe7639f58c7822a
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 23:04:50 -0300

    feat: add validation in postDTO

--- src/main/java/com/project/msblog/dtos/PostDTO.java
+++ src/main/java/com/project/msblog/dtos/PostDTO.java
@@ -1,2 +1,24 @@
-package com.project.msblog.dtos;public class PostDTO {
+package com.project.msblog.dtos;
+
+import com.project.msblog.models.reader.Reader;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PostDTO {
+  @NotNull
+  private Reader author;
+
+  @NotBlank
+  private String title;
+
+  @NotBlank
+  private String description;
+
+  @NotBlank
+  private String category;
+
+  @NotBlank
+  private String content;
 }
