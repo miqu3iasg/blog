68a9821f63979cb13f513384c457cbd72b1e0a79
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-03 23:05:45 -0300

    feat: add methods that searches posts by author and category

--- src/main/java/com/project/msblog/repositories/PostRepository.java
+++ src/main/java/com/project/msblog/repositories/PostRepository.java
@@ -1,11 +1,15 @@
 package com.project.msblog.repositories;
 
 import com.project.msblog.models.post.Post;
+import com.project.msblog.models.reader.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+  List<Post> findByAuthor(Reader author);
+  List<Post> findByCategory(String category);
 }
