7c1566cba0ab388732285f8a0a9cd18fb8b3208d
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-06 15:05:34 -0300

    feat: add implementation of get methods in reader service

--- src/main/java/com/project/msblog/services/reader/ReaderServiceImplementation.java
+++ src/main/java/com/project/msblog/services/reader/ReaderServiceImplementation.java
@@ -1,9 +1,13 @@
 package com.project.msblog.services.reader;
 
+import com.project.msblog.dtos.ReaderDTO;
+import com.project.msblog.exceptions.ReaderNotFoundException;
+import com.project.msblog.models.comment.Comment;
 import com.project.msblog.models.reader.Reader;
 import com.project.msblog.repositories.ReaderRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,12 +20,35 @@ public class ReaderServiceImplementation implements ReaderService {
   }
 
   @Override
+  public List<Reader> getAllReaders() {
+    var readers = readerRepository.findAll();
+
+    if(readers.isEmpty()) throw new ReaderNotFoundException();
+
+    return readers;
+  }
+
+  @Override
+  public List<Comment> getCommentsByReader(UUID readerId) {
+    return null;
+  }
+
+  @Override
   public Optional<Reader> findReaderById(UUID readerId) {
-    return readerRepository.findById(readerId);
+    return Optional.ofNullable(readerRepository.findById(readerId)
+            .orElseThrow(ReaderNotFoundException::new));
   }
 
   @Override
   public Optional<Reader> findReaderByUsername(String username) {
-    return readerRepository.findByUsername(username);
+    return Optional.ofNullable(readerRepository.findByUsername(username)
+            .orElseThrow(ReaderNotFoundException::new));
+  }
+
+  @Override
+  public void deleteReader(UUID readerId) {
+    readerRepository.findById(readerId)
+            .ifPresentOrElse(readerRepository::delete,
+                    () -> { throw new ReaderNotFoundException(); });
   }
 }
