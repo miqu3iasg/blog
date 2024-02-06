6991221544f0568f4dfd698f22182a7f0d4563b2
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-06 15:04:53 -0300

    feat: add signature of get methods for readers in the service

--- src/main/java/com/project/msblog/services/reader/ReaderService.java
+++ src/main/java/com/project/msblog/services/reader/ReaderService.java
@@ -1,11 +1,17 @@
 package com.project.msblog.services.reader;
 
+import com.project.msblog.dtos.ReaderDTO;
+import com.project.msblog.models.comment.Comment;
 import com.project.msblog.models.reader.Reader;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ReaderService {
+  List<Reader> getAllReaders();
+  List<Comment> getCommentsByReader(UUID readerId);
   Optional<Reader> findReaderById(UUID readerId);
   Optional<Reader> findReaderByUsername(String username);
+  void deleteReader(UUID readerId);
 }
