8dd52c08c12d9d906ac48d7b30a60e55380fdeab
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-11 11:47:44 -0300

    feat: add find by email method in reader repository

--- src/main/java/com/project/msblog/repositories/ReaderRepository.java
+++ src/main/java/com/project/msblog/repositories/ReaderRepository.java
@@ -2,6 +2,7 @@ package com.project.msblog.repositories;
 
 import com.project.msblog.models.reader.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface ReaderRepository extends JpaRepository<Reader, UUID> {
   Optional<Reader> findByUsername(String username);
+  UserDetails findByEmail(String email);
 }
