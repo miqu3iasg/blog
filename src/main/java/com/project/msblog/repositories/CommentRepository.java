040230ee0f513a7a5038dfe489949c21b5bc31fd
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-11 12:19:05 -0300

    feat: add method that searches comments by readers

--- src/main/java/com/project/msblog/repositories/CommentRepository.java
+++ src/main/java/com/project/msblog/repositories/CommentRepository.java
@@ -2,6 +2,7 @@ package com.project.msblog.repositories;
 
 import com.project.msblog.models.comment.Comment;
 import com.project.msblog.models.post.Post;
+import com.project.msblog.models.reader.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ import java.util.UUID;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
   List<Comment> findByPost(Post post);
+  List<Comment> findByReader(Reader reader);
 }
