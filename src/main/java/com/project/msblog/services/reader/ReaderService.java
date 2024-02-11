80681ab3188631ea123b9c42b80e80b9da52815b
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-11 11:43:34 -0300

    feat: add method implementations in reader service

--- src/main/java/com/project/msblog/services/reader/ReaderService.java
+++ src/main/java/com/project/msblog/services/reader/ReaderService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ReaderService {
+  Reader registerANewReader(ReaderDTO readerDataRequestForCreate);
   List<Reader> getAllReaders();
   List<Comment> getCommentsByReader(UUID readerId);
   Optional<Reader> findReaderById(UUID readerId);
--- src/main/java/com/project/msblog/services/reader/ReaderServiceImplementation.java
+++ src/main/java/com/project/msblog/services/reader/ReaderServiceImplementation.java
@@ -1,12 +1,17 @@
 package com.project.msblog.services.reader;
 
 import com.project.msblog.dtos.ReaderDTO;
+import com.project.msblog.exceptions.EmailAlreadyExistsException;
 import com.project.msblog.exceptions.ReaderNotFoundException;
+import com.project.msblog.exceptions.UsernameAlreadyExistsException;
 import com.project.msblog.models.comment.Comment;
 import com.project.msblog.models.reader.Reader;
+import com.project.msblog.repositories.CommentRepository;
 import com.project.msblog.repositories.ReaderRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,9 +19,55 @@ import java.util.UUID;
 @Service
 public class ReaderServiceImplementation implements ReaderService {
   private final ReaderRepository readerRepository;
+  private final CommentRepository commentRepository;
 
-  public ReaderServiceImplementation(ReaderRepository readerRepository) {
+  public ReaderServiceImplementation (
+          ReaderRepository readerRepository,
+          CommentRepository commentRepository
+  ) {
     this.readerRepository = readerRepository;
+    this.commentRepository = commentRepository;
+  }
+
+  @Override
+  public Reader registerANewReader(ReaderDTO readerDataRequestForCreate) {
+    validateEmailAndUsernameUniqueness (
+            readerDataRequestForCreate.getEmail(),
+            readerDataRequestForCreate.getUsername()
+    );
+
+    var encryptedPassword = encryptPassword(readerDataRequestForCreate.getPassword());
+
+    var reader = buildReaderModel(readerDataRequestForCreate, encryptedPassword);
+
+    return readerRepository.save(reader);
+  }
+
+  private void validateEmailAndUsernameUniqueness(String email, String username) {
+    if (readerRepository.findByEmail(email) != null) {
+      throw new EmailAlreadyExistsException();
+    }
+
+    if (readerRepository.findByUsername(username) != null) {
+      throw new UsernameAlreadyExistsException();
+    }
+  }
+
+  private String encryptPassword(String password) {
+    return new BCryptPasswordEncoder().encode(password);
+  }
+
+  private Reader buildReaderModel(ReaderDTO readerDataRequestForCreate, String encryptedPassword) {
+    return Reader.builder()
+            .username(readerDataRequestForCreate.getUsername())
+            .firstName(readerDataRequestForCreate.getFirstName())
+            .lastName(readerDataRequestForCreate.getLastName())
+            .email(readerDataRequestForCreate.getEmail())
+            .password(encryptedPassword)
+            .role(readerDataRequestForCreate.getRole())
+            .createdAt(LocalDateTime.now())
+            .updatedAt(LocalDateTime.now())
+            .build();
   }
 
   @Override
@@ -29,8 +80,11 @@ public class ReaderServiceImplementation implements ReaderService {
   }
 
   @Override
-  public List<Comment> getCommentsByReader(UUID readerId) {
-    return null;
+  public List<Comment> getCommentsByReader(UUID readerId ) {
+    var readerExists = readerRepository.findById(readerId)
+            .orElseThrow(ReaderNotFoundException::new);
+
+    return commentRepository.findByReader(readerExists);
   }
 
   @Override
