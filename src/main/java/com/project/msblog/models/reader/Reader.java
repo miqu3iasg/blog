3710ad6a029c487bd95f7fc516fd2bed6a0ca4fd
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-11 11:46:50 -0300

    feat: add override methods of user details in reader model

--- src/main/java/com/project/msblog/models/reader/Reader.java
+++ src/main/java/com/project/msblog/models/reader/Reader.java
@@ -1,15 +1,20 @@
 package com.project.msblog.models.reader;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Data
 @Entity
+@Builder
 @Table(name = "tb_readers")
-public class Reader {
+public class Reader implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private UUID id;
@@ -21,4 +26,33 @@ public class Reader {
   private ReaderRole role;
   private LocalDateTime createdAt;
   private LocalDateTime updatedAt;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if(role == ReaderRole.AUTHOR) return List.of(
+            new SimpleGrantedAuthority("AUTHOR_ROLE"),
+            new SimpleGrantedAuthority("READER_ROLE")
+    );
+    return List.of(new SimpleGrantedAuthority("READER_ROLE"));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
