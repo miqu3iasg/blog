7093b7dc2ab732cd5689bf22eeba34fcf4c00f34
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-02-11 12:19:44 -0300

    feat: add reader controller

--- src/main/java/com/project/msblog/controllers/ReaderController.java
+++ src/main/java/com/project/msblog/controllers/ReaderController.java
@@ -1,2 +1,51 @@
-package com.project.msblog.controllers;public class ReaderController {
+package com.project.msblog.controllers;
+
+import com.project.msblog.dtos.ReaderDTO;
+import com.project.msblog.models.comment.Comment;
+import com.project.msblog.models.reader.Reader;
+import com.project.msblog.services.reader.ReaderService;
+import jakarta.validation.Valid;
+import org.hibernate.annotations.Comments;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/readers")
+public class ReaderController {
+  private final ReaderService readerService;
+
+  public ReaderController(ReaderService readerService) {
+    this.readerService = readerService;
+  }
+
+  @PostMapping("/register")
+  ResponseEntity<Reader> registerANewReader(@RequestBody @Valid ReaderDTO readerDataRequestForCreate) {
+    return new ResponseEntity<>(readerService.registerANewReader(readerDataRequestForCreate), HttpStatus.CREATED);
+  }
+
+  @GetMapping("/list-all-readers")
+  ResponseEntity<List<Reader>> listAllReaders() {
+    return new ResponseEntity<>(readerService.getAllReaders(), HttpStatus.OK);
+  }
+
+  @GetMapping("/list-comments-by-readers/{readerId}")
+  ResponseEntity<List<Comment>> listCommentsByReaders(@PathVariable UUID readerId) {
+    return new ResponseEntity<>(readerService.getCommentsByReader(readerId), HttpStatus.OK);
+  }
+
+  @GetMapping("/search-reader-by-username")
+  ResponseEntity<Optional<Reader>> searchReaderByUsername(@RequestBody String username) {
+    return new ResponseEntity<>(readerService.findReaderByUsername(username), HttpStatus.OK);
+  }
+
+  @DeleteMapping("/remove-reader/{readerId}")
+  ResponseEntity<HttpStatus> removeReader(@PathVariable UUID readerId) {
+    readerService.deleteReader(readerId);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 }
